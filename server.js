c6398134c9669834a3232deef695f06bd039df5d
Iyana Marquez <yayakix@gmail.com>
2022-05-18 20:58:16 -0400

    added color change part

--- server.js
+++ server.js
@@ -2,28 +2,33 @@ const express = require("express");
 const app = express();
 const Budget = require("./models/budget");
 const bodyParser = require("body-parser");
-
+let bankAccount = 0
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 
 
-
+app.get("/budgets/new", (req, res) => {
+  res.render("new.ejs");
+});
 app.get("/budgets", (req, res) => {
-    res.render("index.ejs", {Budget : Budget})
+    bankAccount = 0
+     for (let i = 0; i < Budget.length; i++) {
+       bankAccount += Number(Budget[i].amount);
+     } 
+   
+    res.render("index.ejs", { Budget: Budget, bankAccount: bankAccount });
 });
 
 app.get("/budgets/:index", (req, res) => {
     res.render('show.ejs',  {Budget: Budget[req.params.index]})
 });
 
-app.get("/new", (req, res) => {
-    res.render("new.ejs")
-});
-
 app.post("/budgets", (req, res) => {
-
-    Budget.push(req.body)
+   
+if(req.body.name){
+ Budget.push(req.body);
+}
     res.redirect('/budgets')
 });
 
