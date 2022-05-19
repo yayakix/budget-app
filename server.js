56bd8ba39e27b8374c64f032e08b577a95913cea
Iyana Marquez <yayakix@gmail.com>
2022-05-18 20:02:29 -0400

    new entry page

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const app = express();
 const Budget = require("./models/budget");
+const bodyParser = require("body-parser");
+
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
+
 
 
 app.get("/budgets", (req, res) => {
@@ -14,9 +17,15 @@ app.get("/budgets/:index", (req, res) => {
     res.render('show.ejs',  {Budget: Budget[req.params.index]})
 });
 
-app.get("/budgets/new", (req, res) => {});
+app.get("/new", (req, res) => {
+    res.render("new.ejs")
+});
 
-app.post("/budgets", (req, res) => {});
+app.post("/budgets", (req, res) => {
+
+    Budget.push(req.body)
+    res.redirect('/budgets')
+});
 
 app.listen(3000, () => {
   console.log("server is running on port 3000...");
